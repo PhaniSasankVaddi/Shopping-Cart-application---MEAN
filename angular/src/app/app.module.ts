7170ae0baa3224b72fa78fedb938aca787ec0cc4
Phani Sasank Vaddi <phanisasankvaddi@Phanis-MacBook-Pro.local>
2018-12-01 17:57:37 -0500

    Created angular components and services

---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { UserComponent } from './user/user.component';
+import { AdminComponent } from './admin/admin.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AuthComponent,
+    UserComponent,
+    AdminComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
