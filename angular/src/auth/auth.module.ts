57d6c00237b1ac6b897ae3869ca3797576091f9e
Phani Sasank Vaddi <phanisasankvaddi@Phanis-MacBook-Pro.local>
2018-12-01 20:14:17 -0500

    Created node services for the angular part

--- angular/src/auth/auth.module.ts
+++ angular/src/auth/auth.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoginComponent } from './login/login.component';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
-  declarations: [LoginComponent, SigninComponent, SignupComponent],
+  declarations: [SigninComponent, SignupComponent],
   imports: [
     CommonModule
   ]
