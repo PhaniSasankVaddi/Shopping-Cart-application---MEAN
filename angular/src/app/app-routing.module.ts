3155f5fd09b9c9e69295947ebf5e29740adc6cad
Phani Sasank Vaddi <pvaddi@uwo.ca>
2018-12-08 02:19:43 +0000

    Added Routing to the application

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { FavouriteComponent } from './favourite/favourite.component';
+import { CartComponent } from './cart/cart.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:'items', loadChildren: './modules/shelf/shelf.module#ShelfModule',  
+},
+{
+  path:'adminActions', component: AdminComponent
+},
+{
+  path:'wishlist', component: FavouriteComponent
+},
+{
+  path:'cart', component: CartComponent
+},
+{
+  path:'login', loadChildren: './modules/auth/auth.module#AuthModule',
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular/src/app/modules/shelf/shelf.module.ts
+++ angular/src/app/modules/shelf/shelf.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FruitsComponent } from './fruits/fruits.component';
 import { SearchComponent } from './search/search.component';
+import { ShelfRoutingModule } from './shelf-routing.module';
 
 @NgModule({
   declarations: [FruitsComponent, SearchComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ShelfRoutingModule
   ]
 })
 export class ShelfModule { }
